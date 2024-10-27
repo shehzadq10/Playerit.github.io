12a6f890f450ede54297e5311d57bc09bf9bfb41
shehzadq10 <shehzadq2@icloud.com>
2024-10-27 16:54:32 -0400

    Updated files for social media buttons.
    Replaces all the text of the social media links into their logos that act as a button.

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let messageCount = 0;
+
+function scrollToTop() {
+    const mainContent = document.getElementById('content');
+    mainContent.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+function sendMessage() {
+    document.getElementById('comments').value = ''; // Clear the textarea
+    messageCount++; // Increment message count
+    const messageText = messageCount === 1 ? 'message sent' : 'messages sent';
+    document.getElementById('message').innerText = `(${messageCount}) ${messageText}`; // Display count
+}
